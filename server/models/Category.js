54249dd0eba290174a296f10708735a24bb5dc08
Marcos-GG <marcosdani4125@gmail.com>
2023-10-25 20:12:31 -0300

    model Category

---
+++ server/models/Category.js
@@ -0,0 +1,15 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};

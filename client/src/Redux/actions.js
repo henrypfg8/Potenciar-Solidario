562268995587bdabf288284540ada09aaa4eac20
Mariano <marianoaguirre14@gmail.com>
2023-10-26 17:54:42 -0300

    Actions completas 1.0

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -7,9 +7,11 @@ SEARCH_POST,
 FILTER,
 CLEAR_SEARCH,
 CLEAR_DETAIL,
+CLEAR_QUESTION_DETAIL,
 CREATE_QUESTION,
 DELETE_QUESTION,
 GET_QUESTIONS,
+GET_QUESTION_DETAIL
 } from "./action-types"
 
 import axios from "axios";
@@ -66,6 +68,66 @@ export const searchPost = (query) => {
             dispatch({type: SEARCH_POST, payload: response.data});
         } catch (error) {
             throw new Error(error.response + "por favor contactar a soporte por este error")
+        }
+    }
+}
+
+export const combinedFilter = (filterType, value) => {
+    return {type: FILTER, payload: {filterType, value}}
+}
+
+export const clearSearch = () => {
+    return {type: CLEAR_SEARCH}
+}
+
+export const clearDetail = () => {
+    return {type: CLEAR_DETAIL}
+}
+
+export const clearQuestionDetail = () => {
+    return {type: CLEAR_QUESTION_DETAIL}
+}
+
+export const createQuestion = (question) => {
+    return async function (dispatch) {
+        try {
+            const response = await axios.post("/questions", question);
+            dispatch({type: CREATE_QUESTION, payload: response.data});
+        } catch (error) {
+            throw new Error(error.response + "por favor contactar a soporte por este error");
+        }
+    }
+}
+
+export const deleteQuestion = (id) => {
+    return async function (dispatch) {
+        try {
+            const response = await axios.delete(`/questions/${id}`);
+            dispatch({type: DELETE_QUESTION, payload: response.data});
+        } catch (error) {
+            throw new Error(error.response + "por favor contactar a soporte por este error");
+        }
+    }
+}
+
+export const getQuestions = () => {
+    return async function (dispatch) {
+        try {
+            const response = await axios.get("/questions");
+            dispatch({type: GET_QUESTIONS, payload: response.data})
+        } catch (error) {
+            throw new Error(error.response + "por favor contactar a soporte por este error");
+        }
+    }
+}
+
+export const getQuestionDetail = (id) => {
+    return async function (dispatch) {
+        try {
+            const response = await axios.get(`/questions/${id}`);
+            dispatch({type: GET_QUESTION_DETAIL, payload: response.data})
+        } catch (error) {
+            throw new Error(error.response + "por favor contactar a soporte por este error");
         }
     }
 }

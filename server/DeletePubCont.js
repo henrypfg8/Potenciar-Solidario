b436a66de55c47b94af5e907326d2afe742936b7
enzosamojedny <enzosamojedny@gmail.com>
2023-10-26 18:01:32 -0300

    feat: deletePost feature added

---
+++ server/DeletePubCont.js
@@ -0,0 +1,26 @@
+const DeletePubHandler = require("./DeletePubHandler.js");
+
+const deletePublication = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedRows = await DeletePubHandler(id, userId);
+    if (deletedRows !== 0) {
+      res.status(200).json({
+        message: "La publicación fue borrada exitosamente",
+      });
+    } else {
+      res.status(404).json({
+        message: "Publicacion no encontrada",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Ocurrió un error al intentar borrar la publicación",
+    });
+  }
+};
+
+module.exports = {
+  deletePublication,
+};

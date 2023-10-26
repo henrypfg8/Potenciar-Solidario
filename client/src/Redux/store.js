f42e00d7398cd825ccd4b6b141b5ea6b24b4c2dc
Mariano <marianoaguirre14@gmail.com>
2023-10-26 16:24:56 -0300

    Cambios en store bug fix

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,18 +1,18 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import questionReducer from "./reducers/questionReducer";
-import postReducer from "./reducers/postReducer";
-import searchAndFilterReducer from "./reducers/searchAndFilterReducer";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import questionReducer from "./reducers/questionReducer";
+// import postReducer from "./reducers/postReducer";
+// import searchAndFilterReducer from "./reducers/searchAndFilterReducer";
 import thunk from "redux-thunk";
 
 // Combina los reducers
 const rootReducer = combineReducers({
-  questions: questionReducer,
-  posts: postReducer,
-  search: searchAndFilterReducer,
+  // questions: questionReducer,
+  // posts: postReducer,
+  // search: searchAndFilterReducer,
 });
 
 // Crea la tienda con los reducers combinados
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer(applyMiddleware(thunk)));
 
 export default store;

ea018ee5114e34c2493fdfc75ae9a1fdb803c96f
AngelPenalver <apenalver4@gmail.com>
2023-10-26 10:44:01 -0400

    Forum-Maqueta

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { NavLink, Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
+import Forum from './Forum/Forum'
+import ForumDetail from './ForumDetail/ForumDetail'
+import QuestionCreate from './question/QuestionCreate'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation()
 
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {
+        location.pathname === '/' && <NavLink to='/foro'>
+        <button>Foro</button>
+        </NavLink>
+      }
+      
+      <Routes>
+        <Route path='/foro/create' element={<QuestionCreate/>}/>
+        <Route path='/foro' element={<Forum/>}/>
+        <Route path='/foro/:id' element={<ForumDetail/>}/>  
+      </Routes>
+    </div>
   )
 }
 

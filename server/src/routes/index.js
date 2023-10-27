c58c48df3063d7393832806deb38f854edb73bb7
enzosamojedny <enzosamojedny@gmail.com>
2023-10-26 23:09:33 -0300

    handlers added to routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,7 @@
-const { Router } = require("express");
-
+const { Router } = require("express.js");
+const { login } = require("../authHandler.js");
+const { userPostHandlers } = require("../userPostHandlers.js");
+const { deletePublication } = require("../../DeletePubHand.js");
 // Acá van los handlers
 
 const router = Router();

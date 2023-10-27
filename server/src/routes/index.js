95a8791407bd5f89b139cd0aa27bc28fa06e148b
Marcos-GG <marcosdani4125@gmail.com>
2023-10-27 18:28:34 -0300

    que  salga bien por favor

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -8,20 +8,15 @@ const {
   HandlerGetPublications,
 } = require("../handlers/HandlersPublication.js");
 const { userPost } = require("../handlers/userPost.js");
-const { handlerGetUsers } = require("../handlers/HandlerGetUsers.js");
 
 const router = Router();
 
 // rutas
 router.get("/posts", HandlerGetPublications);
 router.get("/posts/:id", HandlerGetPublications);
-router.get("/users", handlerGetUsers);
-router.get("/users/:id", handlerGetUsers);
-
 router.post("/register", login);
 router.post("/posts", createPublicationHandler);
-router.post("/users", userPost);
-
-router.delete("/posts/:id", deletePublication);
+router.post("/post", userPost);
+router.delete("/post/:id", deletePublication);
 
 module.exports = router;

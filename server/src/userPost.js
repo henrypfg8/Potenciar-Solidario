cbbadc9d978afbe987832ba13bafbe520767ef6c
Marcos-GG <marcosdani4125@gmail.com>
2023-10-26 23:25:22 -0300

    actualizado module.exports

--- server/src/userPost.js
+++ server/src/userPost.js
@@ -1,9 +1,7 @@
-const { userPostHandlers } = require('./userPostHandlers')
+const { userPostHandlers } = require("./userPostHandlers");
 
 const userPost = async (req, res) => {
-    userPostHandlers(req, res);
-}
-       
-module.exports = {
-   userPost
-    };
+  userPostHandlers(req, res);
+};
+
+module.exports = userPost;

6e509a5f57c61681d9ecd8d7349192601de07936
Marcos Antonio Senn <marcossenn@gmail.com>
2023-10-27 18:28:27 -0300

    Update index.js
    "rutas get"

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -8,6 +8,7 @@ const { userPostHandlers } = require("../handlers/PostUser.js");
 const { updateUserHan } = require("../handlers/UpdateUser.js");
 const { DeleteUserHandler } = require("../handlers/DeleteUserH.js");
 const { authRegisterHandler } = require("../handlers/AuthRegisterHandler.js");
+const { handlerGetUsers } = require("../handlers/HandlerGetUsers.js");
 
 const router = Router();
 
@@ -20,5 +21,7 @@ router.get("/posts", HandlerGetPublications);
 router.get("/posts/:id", HandlerGetPublications);
 router.post("/posts", createPublicationHandler);
 router.delete("/posts/:id", DeletePubCont);
+router.get("/users", handlerGetUsers);
+router.get("/users/:id", handlerGetUsers);
 
 module.exports = router;

b7906d1d167c34cb58552b31dd9f2be69fc28059
Mariano <marianoaguirre14@gmail.com>
2023-10-27 15:05:15 -0300

    Delete bug fixed 1.0

--- server/src/controllers/DeletePubCont.js
+++ server/src/controllers/DeletePubCont.js
@@ -2,15 +2,15 @@ const { Publication } = require("../db");
 const { User } = require("../db");
 
 const DeletePubCont = async (id) => {
-  const user = await User.findOne({ where: { id: id } });
+  const user = await Publication.destroy({ where: { id: id } });
 
   if (!user) {
     return { deletedRows: 0, message: "User not found" };
   }
-  const deletedRows = await Publication.destroy({
-    where: { id: id },
-  });
-  return deletedRows;
+  // const deletedRows = await Publication.destroy({
+  //   where: { id: id },
+  // });
+  return user;
 };
 
 module.exports = {DeletePubCont};

0893db065f6e10b802d74efbe95a7b556020b0db
RamiroPagella <ramiro.s.pagella@gmail.com>
2023-10-27 16:33:43 -0300

    Crear publicacion checkkk. falta visualizarlas

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cokieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require('cors')
 const routes  = require('./routes/index')
+
 
 require("./db.js");
 
@@ -10,12 +12,13 @@ const server = express();
 
 server.name = "API";
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cokieParser());
 server.use(morgan("dev"));
 server.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:19789"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:19789", "http://localhost:5173"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",

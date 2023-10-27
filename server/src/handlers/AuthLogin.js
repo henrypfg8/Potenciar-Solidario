5bd5d1cade8261e78c1cd82662f25e4911adf5d4
Marcos Antonio Senn <marcossenn@gmail.com>
2023-10-27 20:44:35 -0300

    "configuracion login y register"

--- server/src/handlers/AuthLogin.js
+++ server/src/handlers/AuthLogin.js
@@ -7,7 +7,7 @@ const authLoginHandler = async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
-        return res.status(400).json({ message: "Ignrese email y contraseña" });
+        return res.status(400).json({ message: "Ingrese email y contraseña" });
     }
 
     try {
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 
 const { DeletePubCont } = require("../controllers/DeletePublication.js");
 const { createPublicationHandler } = require("../handlers/CreatePubHandler.js");
-const { login } = require("../handlers/authHandler.js");
 const { HandlerGetPublications } = require("../handlers/Publication.js");
 const { userPostHandlers } = require("../handlers/PostUser.js");
 const { updateUserHan } = require("../handlers/UpdateUser.js");
@@ -16,6 +15,8 @@ const router = Router();
 // rutas
 router.post("/register", authRegisterHandler);
 router.post("/login", authLoginHandler);
+router.get("/users", handlerGetUsers);
+router.get("/users/:id", handlerGetUsers);
 router.post("/users", userPostHandlers);
 router.put("/users/:id", updateUserHan);
 router.delete("/users/:id", DeleteUserHandler);
@@ -23,7 +24,6 @@ router.get("/posts", HandlerGetPublications);
 router.get("/posts/:id", HandlerGetPublications);
 router.post("/posts", createPublicationHandler);
 router.delete("/posts/:id", DeletePubCont);
-router.get("/users", handlerGetUsers);
-router.get("/users/:id", handlerGetUsers);
+
 
 module.exports = router;

b49d9d0f2d09a0ed0039b0b3dbe4146a1f582a1c
enzosamojedny <enzosamojedny@gmail.com>
2023-10-28 13:12:51 -0300

    PutPublication added

---
+++ server/src/handlers/PutPubH.js
@@ -0,0 +1,45 @@
+const PutPublication = require("../controllers/PutPublication");
+
+const PutPublicationHandler = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    description,
+    category,
+    startDate,
+    endDate,
+    modificationDate,
+    creationDate,
+    status,
+    organization,
+    url,
+    image,
+    registrationLink,
+    contact,
+  } = req.body;
+  try {
+    const putPublication = await PutPublication(id, {
+      title,
+      description,
+      category,
+      startDate,
+      endDate,
+      modificationDate,
+      creationDate,
+      status,
+      organization,
+      url,
+      image,
+      registrationLink,
+      contact,
+    });
+    if (!putPublication)
+      return res
+        .status(401)
+        .json({ msg: "No se ha podido actualizar el post" });
+    return res.status(200).json({ msg: "Post Actualizado" }, putPublication);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+module.exports = { PutPublicationHandler };

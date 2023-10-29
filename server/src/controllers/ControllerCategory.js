e50e196bfd82c3e3994c4f50c7d762b8106ec37d
Marcos-GG <marcosdani4125@gmail.com>
2023-10-29 15:49:12 -0300

    controlador category puede recibir categorias con espacios

--- server/src/controllers/ControllerCategory.js
+++ server/src/controllers/ControllerCategory.js
@@ -1,8 +1,9 @@
 const { Publication } = require("../db.js");
 
 const controllerCategory = async (category) => {
+  const categoryOriginal = category.replace(/[-_]/g, " ");
   const publiCategory = await Publication.findAll({
-    where: { category: category },
+    where: { category: categoryOriginal },
   });
 
   return publiCategory;

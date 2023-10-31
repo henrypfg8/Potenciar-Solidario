a0454a3aff1ee1eebfb26a56e4fdd97b6b6df851
Marcos-GG <marcosdani4125@gmail.com>
2023-10-30 22:02:05 -0300

    get categorias para el select

---
+++ server/src/controllers/getAllCategoriesC.js
@@ -0,0 +1,9 @@
+const { Category } = require("../db");
+
+const controllerCategories = async () => {
+  const allCategories = await Category.findAll();
+
+  return allCategories;
+};
+
+module.exports = { controllerCategories };

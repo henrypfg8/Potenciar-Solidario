60b1b99c65846b5e3280b87ae9dfa83ee5e9432a
elianbenjamin <elianbenjamin30@gmail.com>
2023-10-31 16:18:56 -0300

    bug fix

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db");
 const PORT = process.env.DB_PORT;
 
 conn
-  .sync({ force: false })
+  .sync({ alter: true })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`server conectado a base de datos, puerto ${PORT}`);
--- server/src/controllers/Organization/ControllerGetOngs.js
+++ server/src/controllers/Organization/ControllerGetOngs.js
@@ -3,7 +3,7 @@ const { Publication } = require("../../db.js");
 const controllerOngs = async (ongs) => {
   const ongsOriginal = ongs.replace(/[-_]/g, " ");
   const publications = await Publication.findAll({
-    where: { ongs: ongsOriginal },
+    where: { organization: ongsOriginal },
   });
 
   return publications;

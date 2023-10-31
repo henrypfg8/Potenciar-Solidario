bf6dfb5de80da403aa8a3a2ca8166df15abf4a17
Marcos Antonio Senn <marcossenn@gmail.com>
2023-10-31 17:36:46 -0300

    agrego validacion de google y corrigo register de usuarios

--- server/src/handlers/Authentication/authGoogleLoginHandler.js
+++ server/src/handlers/Authentication/authGoogleLoginHandler.js
@@ -1,21 +1,29 @@
 const { User } = require("../../db");
 require("dotenv").config();
-import { OAuth2Client } from "google-auth-library";
+const { OAuth2Client } = require("google-auth-library");
 
 const authGoogleHandler = async (req, res) => {
     const client = new OAuth2Client(process.env.CLIENT_ID);
     const { idToken } = req.body;
 
     try {
-        const response = await client.verifyIdToken({ idToken, audience: process.env.CLIENT_ID })
+        const response = await client.verifyIdToken({
+            idToken,
+            audience: process.env.CLIENT_ID,
+        });
         const { email } = response.payload;
 
         const exist = await User.findOne({ where: { email: email } });
-        
+
         if (exist) {
             return res.status(200).json({ message: "Acceso concedido" });
         } else {
-            return res.status(400).json({ message: "El correo electrónico no está registrado, por favor regístrese" });
+            return res
+                .status(400)
+                .json({
+                    message:
+                        "El correo electrónico no está registrado, por favor regístrese",
+                });
         }
     } catch (error) {
         console.log(error.message);

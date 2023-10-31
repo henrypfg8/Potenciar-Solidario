ce7ef8825c36eb87d005505002aa91b896d8b184
RamiroPagella <ramiro.s.pagella@gmail.com>
2023-10-31 14:48:02 -0300

    se traen las categories cuando se inicia la app

--- client/src/components/LeftBar/LeftBar.jsx
+++ client/src/components/LeftBar/LeftBar.jsx
@@ -1,24 +1,19 @@
 import Styles from "./leftBar.module.css";
 //
 import { useLocation, Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+//
 import Select from "react-select";
+//
 
 export default function LeftBar() {
   const { pathname } = useLocation();
 
 
+
+
   const categoryOptions = [
-    { label: "Filtrar por categoria", value: "Todas las categorias" },
-    { label: "Donaciones ofrecidas", value: "Donaciones ofrecidas" },
-    { label: "Donaciones pedidas", value: "Donaciones Pedidas" },
-    { label: "Capacitaciones", value: "Capacitaciones" },
-    { label: "Concursos", value: "Concursos" },
-    { label: "Busquedas laborales", value: "Ofrecimientos laborales" },
-    { label: "Ofrecimientos laborales", value: "Ofrecimientos laborales" },
-    { label: "Emprendimientos de la ONG", value: "Emprendimientos de la ONG" },
-    { label: "Eventos de la ONG", value: "Eventos de la ONG" },
-    { label: "Contactos Utiles", value: "Contactos Utiles"}
+
   ];
   const dateOptions = [
     { label: "Filtrar por fecha", value: "Todas las fechas" },

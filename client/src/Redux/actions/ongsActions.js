184ee998e14079b9bd1a6edaf823968f703d80c5
Mariano <marianoaguirre14@gmail.com>
2023-11-01 14:57:01 -0300

    Bug fixes

--- client/src/Redux/actions/ongsActions.js
+++ client/src/Redux/actions/ongsActions.js
@@ -19,7 +19,7 @@ export const getOngs = () => {
 export const getPostsByOngs = (query) => {
     return async function (dispatch) {
         try {
-            const response = await axios.get(`http://localhost:19789/ongs?name=${query}`);
+            const response = await axios.get(`http://localhost:19789/ongs/filter?ongs=${query}`);
             dispatch({type: GET_POSTS_BY_ONGS, payload: response.data});
         } catch (error) {
             console.log(error, "por favor contactar a soporte por este error")

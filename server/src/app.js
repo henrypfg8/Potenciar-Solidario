670c9b29424ce844a680363d2b16e6735d01cd8b
enzosamojedny <enzosamojedny@gmail.com>
2023-11-01 13:17:28 -0300

    socket.io import added

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cokieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const cors = require('cors')
-const routes  = require('./routes/index')
-
+const cors = require("cors");
+const routes = require("./routes/index");
+const io = require("socket.io")(server);
 
 require("./db.js");
 
@@ -12,13 +12,17 @@ const server = express();
 
 server.name = "API";
 
-server.use(cors())
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cokieParser());
 server.use(morgan("dev"));
 server.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:19789", "http://localhost:5173"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:19789",
+    "http://localhost:5173"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
@@ -28,7 +32,7 @@ server.use((_req, res, next) => {
   next();
 });
 
-server.use('/', routes)
+server.use("/", routes);
 
 server.use((err, _req, res, _next) => {
   // eslint-disable-line no-unused-vars

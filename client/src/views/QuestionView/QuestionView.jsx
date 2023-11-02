abba49ea07de4612682e4176561625cc3bfdffc9
Angel Peñalver <apenalver4@gmail.com>
2023-11-02 14:59:43 -0400

    Update QuestionView.jsx

--- client/src/views/QuestionView/QuestionView.jsx
+++ client/src/views/QuestionView/QuestionView.jsx
@@ -6,7 +6,7 @@ import { FlechaParaArriba } from '../../assets/FlechaParaArribaIcon';
 import io from 'socket.io-client'
 const socket = io('/')
 
-function QuestionView({question}) {
+function QuestionView({ question }) {
     const [view, setView] = useState({});
     const [messages, setMessages] = useState('')
     const [answers, setAnswers] = useState('')
@@ -21,14 +21,14 @@ function QuestionView({question}) {
     const handleSubmit = (message) => {
         socket.emit('message', message)
         event.preventDefault()
-        
+
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         socket.on("message", message => {
             console.log(message);
             setMessages([...messages, message])
-            });
+        });
 
     }, [])
     const handleView = (id) => {
@@ -38,12 +38,12 @@ function QuestionView({question}) {
         })
         )
     }
-    useEffect(()=>{
+    useEffect(() => {
         socket.on('message', message => {
 
             console.log(message);
         });
-    },[])
+    }, [])
 
     console.log(question);
 
@@ -51,71 +51,71 @@ function QuestionView({question}) {
         <div >
             {
                 question ?
-                <div className={style.container}>
+                    <div className={style.container}>
 
-            <div className={style.div1}>
-                <div >
+                        <div className={style.div1}>
+                            <div >
 
-                    <h1>{question?.title}</h1>
-                    <div className={style.date}>
-                        <a>Publicado: <h5>20-01-2021</h5></a>
-                    </div>
-                    {/* <h3>{question.username}</h3> */}
-                    <p>{question.text}</p>
-                </div>
-            </div>
-
-            <div className={style.contain}>
-                {
-                    question.Answers.length > 0 ?
-                        <div className={style.title}>
-                            <h2>
-                                {question.Answers.length > 1 ? <h2><p>{question.Answers.length}</p> Respuestas</h2> : <
-                                    h2><p>1</p>Respuesta</h2>}
-
-                            </h2>
+                                <h1>{question?.title}</h1>
+                                <div className={style.date}>
+                                    <a>Publicado: <h5>20-01-2021</h5></a>
+                                </div>
+                                {/* <h3>{question.username}</h3> */}
+                                <p>{question.text}</p>
+                            </div>
                         </div>
-                        :
-                        <div>
-                            <h2>No hay respuestas</h2>
-                        </div>
-                }
 
-            {question.Answers?.map((respuesta, index) => {
-                    return (
-                        <div key={index} className={style.response}>
+                        <div className={style.contain}>
+                            {
+                                question.Answers.length > 0 ?
+                                    <div className={style.title}>
+                                        <h2>
+                                            {question.Answers.length > 1 ? <h2><p>{question.Answers.length}</p> Respuestas</h2> : <
+                                                h2><p>1</p>Respuesta</h2>}
 
-                            <p>{respuesta.text}</p>
-                            <h4>  
-                                {/* {usuariosRespuestas.map(usuario => {
+                                        </h2>
+                                    </div>
+                                    :
+                                    <div>
+                                        <h2>No hay respuestas</h2>
+                                    </div>
+                            }
+
+                            {question.Answers?.map((respuesta, index) => {
+                                return (
+                                    <div key={index} className={style.response}>
+
+                                        <p>{respuesta.text}</p>
+                                        <h4>
+                                            {/* {usuariosRespuestas.map(usuario => {
                                     if (usuario.id === respuesta.id) {
                                         return usuario.username;
                                     }
                                 })} */}
-                            </h4>
+                                        </h4>
 
 
-                            {!view[index] ?  <a onClick={() => handleView(index)}>Añadir comentario<FlechaAbajoIcon/></a> : <a onClick={() => handleView(index)}>Añadir comentario<FlechaParaArriba/></a>
-                            }
-                           
-                            {view[index] && <div className={style.comment}>
-                            <p>Comentar</p>
-                                <textarea type="text" cols="6" rows="5" onChange={()=>handleChange(event)} />
-                                <button onClick={()=> handleSubmit(messages)}>Añadir comentario</button>
-                            </div>}
+                                        {!view[index] ? <a onClick={() => handleView(index)}>Añadir comentario<FlechaAbajoIcon /></a> : <a onClick={() => handleView(index)}>Añadir comentario<FlechaParaArriba /></a>
+                                        }
+
+                                        {view[index] && <div className={style.comment}>
+                                            <p>Comentar</p>
+                                            <textarea type="text" cols="6" rows="5" onChange={() => handleChange(event)} />
+                                            <button onClick={() => handleSubmit(messages)}>Añadir comentario</button>
+                                        </div>}
+                                    </div>
+                                )
+                            })}
+                            <div className={style.question}>
+                                <textarea type="text" rows="8" />
+                                <button>Responder</button>
+                            </div>
                         </div>
-                    )
-                })}
-                <div className={style.question}>
-                    <textarea type="text" rows="8" />
-                    <button>Responder</button>
-                </div> 
-            </div> 
-                </div>
-                :
-                <div>
-                    Cargando
-                </div>
+                    </div>
+                    :
+                    <div>
+                        Cargando
+                    </div>
             }
         </div>
     )

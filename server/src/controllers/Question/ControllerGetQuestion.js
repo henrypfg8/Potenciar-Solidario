d7bcfd673cefaf90a64aebe271f9d741b52dcf1b
Marcos-GG <marcosdani4125@gmail.com>
2023-11-02 15:15:35 -0300

    ahora questions  y answer incluyen el nombre del ususario

--- server/src/controllers/Question/ControllerGetQuestion.js
+++ server/src/controllers/Question/ControllerGetQuestion.js
@@ -1,17 +1,24 @@
-const { Question, Answer } = require("../../db.js");
+const { Question, Answer , User} = require("../../db.js");
 
 const ControllerGetQuestion = async (id) => {
   if (id) {
     const questionById = await Question.findOne({
       where: { id: id },
-      include: { model: Answer },
+      include: [
+        { model: Answer , include: {model: User , attributes: ['name']}},
+        {model: User, attributes: ['name']}
+      ]
+      
     });
 
     return questionById;
   }
 
   const questionAll = await Question.findAll({
-    include: { model: Answer },
+    include: [
+      { model: Answer , include: {model: User , attributes: ['name']}},
+      {model: User, attributes: ['name']}
+    ]
   });
 
   return questionAll;

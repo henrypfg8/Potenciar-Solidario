945fb57d588395bb3a2c377a6f47c839c90c8e68
Angel Peñalver <apenalver4@gmail.com>
2023-11-03 08:25:10 -0400

    update foro

--- server/src/handlers/Aswer/CreateAnswer.js
+++ server/src/handlers/Aswer/CreateAnswer.js
@@ -1,15 +1,16 @@
 const { CreateAnswer } = require("../../controllers/Aswer/CreateAnswer");
 
 const createAnswerHandler = async (req, res) => {
-  const { answer } = req.body;
-  try {
-    if (!answer) {
-      throw new Error("La answer es obligatoria");
-    }
-    const Answer = await CreateAnswer({ answer });
-    res.status(201).json(Answer);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+  const { answer, userId, questionId } = req.body;
+try {
+  if (!answer || !userId || !questionId) {
+    throw new Error("La answer, userId y questionId son obligatorios");
   }
+  const Answer = await CreateAnswer({ answer, userId, questionId });
+  res.status(201).json(Answer);
+} catch (error) {
+  res.status(400).json({ error: error.message });
+}
+
 };
 module.exports = { createAnswerHandler };

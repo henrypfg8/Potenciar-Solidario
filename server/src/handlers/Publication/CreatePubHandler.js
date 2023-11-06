a009b2ecdfc97ae543d7628fff6420784ccae6e9
Marcos Antonio Senn <marcossenn@gmail.com>
2023-11-06 19:23:03 -0300

    Update CreatePubHandler.js

--- server/src/handlers/Publication/CreatePubHandler.js
+++ server/src/handlers/Publication/CreatePubHandler.js
@@ -1,4 +1,5 @@
 const {CreatePublication} = require("../../controllers/Publication/CreatePublication");
+const {Category} = require('../../db.js');
 
 const createPublicationHandler = async (req, res) => {
     const {
@@ -21,7 +22,20 @@ const createPublicationHandler = async (req, res) => {
     const userId = req.userId;
     console.log(`este es el user id ${userId}`); 
 
+
+
     try {
+        console.log(category)
+        const categorySearch = await Category.findOne({ where: { name: category } });
+        console.log(categorySearch)
+        let categoryId = null;
+        if(categorySearch){
+            categoryId = categorySearch.id;
+            console.log(categoryId)
+        }
+        
+       
+
         if (!title || !description || !category || !contact || !organization)
             throw new Error("Faltan campos obligatorios");
 
@@ -41,7 +55,8 @@ const createPublicationHandler = async (req, res) => {
             registrationLink,
             contact,
             likes, 
-            userID:userId
+            userID:userId,
+            categoryId:categoryId
         });
 
         res.status(201).json(publication);

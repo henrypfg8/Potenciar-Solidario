7dbc66672508a935aa760ee5f1705d9fb1020bc5
Marcos Antonio Senn <marcossenn@gmail.com>
2023-11-06 17:47:34 -0300

    se agrega userID cuando se crea una publicacion

--- server/src/handlers/Publication/CreatePubHandler.js
+++ server/src/handlers/Publication/CreatePubHandler.js
@@ -18,8 +18,8 @@ const createPublicationHandler = async (req, res) => {
          likes 
     } = req.body;
 
-    //const userId = req.userId;
-    //console.log(`este es el user id ${userId}`); 
+    const userId = req.userId;
+    console.log(`este es el user id ${userId}`); 
 
     try {
         if (!title || !description || !category || !contact || !organization)
@@ -41,7 +41,7 @@ const createPublicationHandler = async (req, res) => {
             registrationLink,
             contact,
             likes, 
-            //userID:userId
+            userID:userId
         });
 
         res.status(201).json(publication);

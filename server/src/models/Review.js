7942730969e222c22aa43c1b015cc3634bf03a48
enzosamojedny <enzosamojedny@gmail.com>
2023-11-07 17:45:08 -0300

    Reviews added

---
+++ server/src/models/Review.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "Review",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        validate: {
+          isInt: true,
+          min: 1,
+        },
+      },
+
+      review: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [20, 1000],
+        },
+      },
+    }
+  );
+};

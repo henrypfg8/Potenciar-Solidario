06d76c9c747bd1edee42dbc7af773ead5ffea057
Marcos-GG <marcosdani4125@gmail.com>
2023-11-07 11:01:26 -0300

    modelo comment  a Answer

--- server/src/controllers/Aswer/GetAnswer.js
+++ server/src/controllers/Aswer/GetAnswer.js
@@ -1,8 +1,12 @@
 const { Answer } = require("../../db");
+const {Comment} = require('../../db')
 
 const GetAnswer = async (id) => {
     
-    const allAnswer = await Answer.findAll({ where: id})
+    const allAnswer = await Answer.findAll(
+        { where: { id: id},
+        include : {model: Comment}
+    })
     if(!allAnswer) throw new Error('No se pudo traer las preguntas.')
     return allAnswer;
 

878bcd1d9a406c1fb42819580f157ae4e098a5f6
enzosamojedny <enzosamojedny@gmail.com>
2023-11-07 18:04:08 -0300

    reviews terminadas
    FALTA DEBUGGEARRRRRRRRRRRRRR

--- server/src/controllers/Reviews/CreateReviewC.js
+++ server/src/controllers/Reviews/CreateReviewC.js
@@ -1,1 +1,13 @@
-const {}
+const { Review } = require("../../db");
+
+const CreateReviewC = async ({ review, userId, publicationId }) => {
+  const newReview = await Review.create({ review, userId, publicationId });
+  if (!newReview) {
+    throw new Error("No se pudo crear el review");
+  }
+
+//global.io.emit(`question_${publicationId}`);//!
+
+  return newReview;
+};
+module.exports = { CreateReviewC };

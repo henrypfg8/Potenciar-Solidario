5b16b3e6cfe99dd4bc48a977a9930f8f13756076
Marcos-GG <marcosdani4125@gmail.com>
2023-11-07 21:35:48 -0300

    se suma el valor de los likes al hacer un post

--- server/src/controllers/Publication/PostLike.js
+++ server/src/controllers/Publication/PostLike.js
@@ -9,6 +9,15 @@ const PostLike = async ({ idUser, idPublication}) => {
 
   if (!like) throw new Error("No se pudo dar like a la publicacion.");
 
+ const publication = await Publication.findOne({
+  where: {id : idPublication}
+ });
+
+ if(publication.likes >= 0) {
+  publication.likes = publication.likes + 1 
+  await publication.save();
+ }
+ 
   return like;
 };
 module.exports = {PostLike};

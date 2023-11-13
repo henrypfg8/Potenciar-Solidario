0562324724a9146768a9596db030b60b4a3af416
Marcos-GG <marcosdani4125@gmail.com>
2023-11-13 02:53:53 -0300

    create Comment

--- server/src/controllers/Comments/CreateCommentC.js
+++ server/src/controllers/Comments/CreateCommentC.js
@@ -1,6 +1,6 @@
 const { Comment } = require("../../db");
 
-const CreateComment = async ({thread, userId, answerId, questionId}) => {//!
+const CreateComment = async ({thread, userId, answerId, questionId}) => {
   const newComment = await Comment.create({thread, userId, answerId});
   console.log('controllers', newComment)
   if (!newComment) throw new Error("No se pudo crear el comentario.");

3765ee41d44235f03169155ee602fa121c04fc04
enzosamojedny <enzosamojedny@gmail.com>
2023-11-13 18:53:54 -0300

    cambios varios

--- client/src/components/Notifications/Notifications.jsx
+++ client/src/components/Notifications/Notifications.jsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { notification } from 'antd';
 import { createPortal } from 'react-dom';
-import { SmileOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom'
 import './notification.css';
+
 const Notifications = () => {
+    const navigate = useNavigate();
     const answerComments = useSelector(state => state.answerComments);
     const [api, contextHolder] = notification.useNotification();
     useEffect(() => {
@@ -17,13 +19,11 @@ const Notifications = () => {
                     (element2) => element2.id === element.id
                 )
         );
-
         newNotifications?.forEach((newNotification) => {
             const notificationContent = (
-                <div style={{ border: '1px solid black', backgroundColor: 'yellow' }}>
-                    <a href={`/questions/${newNotification?.QuestionId}`}><p>{`New answer from ${newNotification?.User?.name}`}</p>
-                        <p>{newNotification?.content}</p>
-                    </a>
+                <div>
+                    <p>{`New answer from ${newNotification?.User?.name}`}</p>
+                    <p>{newNotification?.content}</p>
                 </div>
             );
 
@@ -33,25 +33,15 @@ const Notifications = () => {
             api.open({
                 message: 'Notification',
                 description: createPortal(notificationContent, notificationContainer),
+                onClick: () => {
+                    navigate(`/questions/${newNotification?.QuestionId}`);
+                },
                 onClose: () => {
                     document.body.removeChild(notificationContainer);
                     console.log('Notification was closed.');
                 },
-                onClick: () => {
-                    console.log('Notification was clicked.');
-                },
-                icon: (
-                    <SmileOutlined />
-                ),
-                type: 'info',
             });
         });
-
-        const handleClick = (event, path) => {
-
-            console.log('Handling click event:', event);
-
-        };
     }, [answerComments]);
 
     return null;
